bafde9aac19349a4fe1f52f05d9c1021a4a4a093
bhaktiraichura <bhaktiraichura1995@gmail.com>
2023-05-10 16:58:01 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,59 +1,60 @@
 import torch
 from torch.utils.data import DataLoader
-from dataset import TextDataset
+from dataset import Seq2SeqDataset
 from encoder import Encoder
 from decoder import Decoder
 
-# Define the path to the saved model
-MODEL_PATH = "model.pt"
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Define the batch size to use for testing
-BATCH_SIZE = 32
+# Load dataset
+test_dataset = Seq2SeqDataset('test.txt')
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
-# Define the device to use for testing (CPU or GPU)
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Load models
+encoder = Encoder(input_size=128, hidden_size=256).to(device)
+decoder = Decoder(output_size=128, hidden_size=256).to(device)
 
-# Load the test dataset
-test_dataset = TextDataset("test.txt")
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
+encoder.load_state_dict(torch.load('encoder.pth'))
+decoder.load_state_dict(torch.load('decoder.pth'))
 
-# Load the saved model
-encoder = Encoder(input_size=len(test_dataset.vocab), hidden_size=256)
-decoder = Decoder(output_size=len(test_dataset.vocab), hidden_size=256)
-model = torch.nn.Sequential(encoder, decoder).to(DEVICE)
-model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
+encoder.eval()
+decoder.eval()
 
-# Evaluate the model on the test dataset
-model.eval()
+# Test loop
+total_loss = 0
 with torch.no_grad():
-    total_loss = 0
-    total_correct = 0
-    total_predictions = 0
-    for input_seq, target_seq in test_loader:
-        input_seq = input_seq.to(DEVICE)
-        target_seq = target_seq.to(DEVICE)
-        
-        encoder_hidden = encoder.init_hidden(BATCH_SIZE)
+    for i, (input_seq, target_seq) in enumerate(test_loader):
+        # Move data to device
+        input_seq = input_seq.to(device)
+        target_seq = target_seq.to(device)
+
+        # Encode input sequence
+        encoder_hidden = encoder.init_hidden()
         encoder_outputs, encoder_hidden = encoder(input_seq, encoder_hidden)
+
+        # Initialize decoder input
+        decoder_input = torch.tensor([[0]], device=device)
+
+        # Initialize decoder hidden state with encoder final hidden state
         decoder_hidden = encoder_hidden
-        decoder_input = target_seq[:, 0].unsqueeze(1)
-        
-        loss = 0
-        correct = 0
-        predictions = 0
-        for t in range(1, target_seq.size(1)):
-            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden, encoder_outputs)
-            loss += torch.nn.functional.cross_entropy(decoder_output, target_seq[:, t])
-            _, topi = decoder_output.topk(1)
-            correct += (topi.squeeze() == target_seq[:, t]).sum().item()
-            predictions += target_seq.size(0)
-            decoder_input = topi.detach()
-        
-        total_loss += loss.item() * target_seq.size(0)
-        total_correct += correct
-        total_predictions += predictions
-    
-    avg_loss = total_loss / len(test_dataset)
-    accuracy = total_correct / total_predictions
-    f1_score = 2 * (accuracy * (1 - accuracy)) / (accuracy + (1 - accuracy))
-    print("Test Loss: {:.4f}, Accuracy: {:.4f}, F1 Score: {:.4f}".format(avg_loss, accuracy, f1_score))
+
+        # Decode sequence
+        decoded_seq = []
+        for t in range(target_seq.size(1)):
+            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            decoded_seq.append(decoder_output.argmax(1).item())
+            decoder_input = target_seq[:, t].unsqueeze(1)
+
+        # Calculate loss
+        loss = torch.nn.functional.cross_entropy(torch.tensor(decoded_seq, device=device), target_seq.squeeze(0))
+        total_loss += loss.item()
+
+        # Print decoded sequence
+        print(f"Input Sequence: {input_seq.tolist()[0]}")
+        print(f"Target Sequence: {target_seq.tolist()[0]}")
+        print(f"Decoded Sequence: {decoded_seq}")
+        print(f"Loss: {loss.item()}")
+
+# Print average loss
+print(f"Average Loss: {total_loss / len(test_loader)}")
